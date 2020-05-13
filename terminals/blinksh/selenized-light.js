90f578ededf7f2c6ff1d30e061b0705cfc776102
Dylan Hardison <dylan@hardison.net>
2020-05-13 13:56:31 -0400

    re-run evaluate-template for the terminals I care about

--- terminals/blinksh/selenized-light.js
+++ terminals/blinksh/selenized-light.js
@@ -1,4 +1,4 @@
-t.prefs_.set('color-palette-overrides',["#ece3cc", "#83c746", "#83c746", "#83c746", "#83c746", "#83c746", "#83c746", "#83c746", "#83c746", "#83c746", "#83c746", "#83c746", "#83c746", "#83c746", "#83c746", "#83c746"]);
+t.prefs_.set('color-palette-overrides',["#e9e4d0", "#d2212d", "#489100", "#ad8900", "#0072d4", "#ca4898", "#009c8f", "#909995", "#cfcebe", "#cc1729", "#428b00", "#a78300", "#006dce", "#c44392", "#00978a", "#3a4d53"]);
 t.prefs_.set('foreground-color', "#53676d");
 t.prefs_.set('background-color', "#fbf3db");
 t.prefs_.set('cursor-color', 'rgba(0,0,0,0.5)');
